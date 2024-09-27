65ac01154763ad96c41d1098807e8b61b5ac21a2
Carlos Mendez <carlos.mendez@unesp.br>
2024-09-27 14:34:14 -0300

    Update Precipitation and rainfall.js
    Signed-off-by: Carlos Mendez <carlos.mendez@unesp.br>

--- 03. Precipitation and rainfall/Precipitation and rainfall.js
+++ 03. Precipitation and rainfall/Precipitation and rainfall.js
@@ -48,5 +48,65 @@ var chart = ui.Chart.image.series({
 })
 print(chart)
 
+////////////////////////////////////////////////////////////////// AVERAGE RAINFALL IN A BASIN  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var chirps = ee.ImageCollection("UCSB-CHG/CHIRPS/PENTAD");
+var hydrobasins = ee.FeatureCollection("WWF/HydroSHEDS/v1/Basins/hybas_4");
+
+var basin = hydrobasins.filter(ee.Filter.eq('HYBAS_ID', 6040752470))
+var startDate = ee.Date.fromYMD(2022, 1,1)
+var endDate = startDate.advance(1, 'year')
+var filtered = chirps.filter(ee.Filter.date(startDate, endDate))
+
+// Calculate yearly rainfall
+var total = filtered.reduce(ee.Reducer.sum())
+var palette = ['#ffffcc','#a1dab4','#41b6c4','#2c7fb8','#253494']
+var visParams = {min:0, max: 2000, palette: palette}
+
+Map.addLayer(total, visParams, 'Total Precipitation')
+Map.addLayer(basin, {}, 'Basin')
+
+Map.centerObject(basin)
+
+// Calculate average rainfall 
+var stats = total.reduceRegion({ reducer: ee.Reducer.mean(), geometry: basin, scale: 5000, })
+print(stats)
+print('Average Rainfall:', stats.get('precipitation_sum'))
 
 
+////////////////////////////////////////////////////////////////// ANUAL RAINFALL BY COUNTRY  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var chirps = ee.ImageCollection('UCSB-CHG/CHIRPS/DAILY')
+             .filterDate('2020-01-01', '2020-12-31')
+             .select('precipitation');
+
+var annualPrecipitation = chirps.sum();
+var countries = ee.FeatureCollection("FAO/GAUL/2015/level0");
+var pakistan = countries.filter(ee.Filter.eq('ADM0_NAME', 'Mexico'));
+
+var clippedPrecipitation = annualPrecipitation.clip(pakistan);
+var precipitationVis = {min: 0, max: 1000, palette: ['white', 'blue', 'cyan', 'green', 'yellow', 'red']};
+var legend = ui.Panel({ style: { position: 'bottom-right', padding: '8px 15px'}});
+var legendColors = precipitationVis.palette;
+var legendLabels = ['0', '100', '200', '300', '400', '500+'];
+
+var makeLegend = function(colors, labels) {
+  var entry = colors.map(function(color, index) {
+    return ui.Panel({
+      style: {
+        backgroundColor: color, padding: '8px 10px', margin: '0 0 4 px 0'},
+      widgets:
+      [
+        ui.Label({
+          value: labels[index], style: {color: 'black'}
+                })
+      ]
+    });
+  });
+  return ui.Panel(entry);
+};
+
+Map.centerObject(pakistan, 4);
+Map.addLayer(clippedPrecipitation, precipitationVis, 'Annual Precipitation (mm)');
+legend.add(makeLegend(legendColors, legendLabels));
+Map.add(legend);
