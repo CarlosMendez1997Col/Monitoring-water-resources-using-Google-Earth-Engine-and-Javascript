8ef6c3303ee47b0655a7b37638d5276aab1f7392
Carlos Mendez <carlos.mendez@unesp.br>
2024-10-06 20:05:46 -0300

    Update Precipitation and rainfall.js
    Signed-off-by: Carlos Mendez <carlos.mendez@unesp.br>

--- 03. Precipitation and rainfall/Precipitation and rainfall.js
+++ 03. Precipitation and rainfall/Precipitation and rainfall.js
@@ -1,18 +1,16 @@
 ////////////////////////////////////////////////////////////////// TIME SERIES OF PRECIPITATION  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// select feature collection of images
 var hydrobasins = ee.FeatureCollection("WWF/HydroSHEDS/v1/Basins/hybas_4");
 var Basin = hydrobasins.filter(ee.Filter.eq('HYBAS_ID', 6040752470))
-
-Map.centerObject(Basin, 5);
-Map.addLayer(Basin, {}, 'Basin');
-
+// filter data by calendar
 var startYear = 2010;
 var endYear = 2020;
 var startDate = ee.Date.fromYMD(startYear, 1, 1);
 var endDate = ee.Date.fromYMD(endYear + 1, 1, 1);
 var years = ee.List.sequence(startYear, endYear);
 var months = ee.List.sequence(1, 12);
+//select CHIRPS dataset
 var CHIRPS = ee.ImageCollection('UCSB-CHG/CHIRPS/PENTAD');
-
 CHIRPS = CHIRPS.filterDate(startDate, endDate);
 
 var monthlyPrecip = ee.ImageCollection.fromImages(
@@ -31,8 +29,6 @@ var monthlyPrecip = ee.ImageCollection.fromImages(
 );
 
 var precipVis = {min: 0, max: 250, palette: 'white, blue, darkblue, red, purple'};
-Map.addLayer(monthlyPrecip.mean().clip(Basin), precipVis, '2015 precipitation');
-
 var title = {title: 'Monthly precipitation', hAxis: {title: 'Time'}, vAxis: {title: 'Precipitation (mm)'},
 };
 
@@ -49,24 +45,24 @@ var chartMonthly = ui.Chart.image.seriesByRegion({
 
 print(chartMonthly);
 
+Map.setOptions('SATELLITE')
+Map.centerObject(Basin, 5);
+Map.addLayer(Basin, {}, 'Basin');
+Map.addLayer(monthlyPrecip.mean().clip(Basin), precipVis, '2015 precipitation');
+
 ////////////////////////////////////////////////////////////////// MONTHLY RAINFALL  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 var chirps = ee.ImageCollection("UCSB-CHG/CHIRPS/PENTAD");
 var hydrobasins = ee.FeatureCollection("WWF/HydroSHEDS/v1/Basins/hybas_4");
 var basin = hydrobasins.filter(ee.Filter.eq('HYBAS_ID', 6040752470));
-
 var geometry = basin;
-Map.addLayer(geometry, {}, 'POI')
-
 var year = 2023
 var startDate = ee.Date.fromYMD(year, 1, 1)
 var endDate = startDate.advance(1, 'year')
-var yearFiltered = chirps
-  .filter(ee.Filter.date(startDate, endDate))
+var yearFiltered = chirps.filter(ee.Filter.date(startDate, endDate))
 print(yearFiltered)
 
 var months = ee.List.sequence(1, 12)
-
 var createMonthlyImage = function(month) {
   var startDate = ee.Date.fromYMD(year, month, 1)
   var endDate = startDate.advance(1, 'month')
@@ -93,17 +89,17 @@ var chart = ui.Chart.image.series({
 }).setOptions({
       lineWidth: 1,
       pointSize: 3,
-      title: 'Monthly Rainfall at Bengaluru',
+      title: 'Monthly Rainfall in Paranapanema River Basin',
       vAxis: {title: 'Rainfall (mm)'},
       hAxis: {title: 'Month', gridlines: {count: 12}}
 })
 print(chart)
-
+Map.setOptions('SATELLITE')
+Map.addLayer(geometry, {}, 'PRB')
 ////////////////////////////////////////////////////////////////// AVERAGE RAINFALL IN A BASIN  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 var chirps = ee.ImageCollection("UCSB-CHG/CHIRPS/PENTAD");
 var hydrobasins = ee.FeatureCollection("WWF/HydroSHEDS/v1/Basins/hybas_4");
-
 var basin = hydrobasins.filter(ee.Filter.eq('HYBAS_ID', 6040752470))
 var startDate = ee.Date.fromYMD(2022, 1,1)
 var endDate = startDate.advance(1, 'year')
@@ -114,17 +110,15 @@ var total = filtered.reduce(ee.Reducer.sum())
 var palette = ['#ffffcc','#a1dab4','#41b6c4','#2c7fb8','#253494']
 var visParams = {min:0, max: 2000, palette: palette}
 
-Map.addLayer(total, visParams, 'Total Precipitation')
-Map.addLayer(basin, {}, 'Basin')
-
-Map.centerObject(basin)
-
 // Calculate average rainfall 
 var stats = total.reduceRegion({ reducer: ee.Reducer.mean(), geometry: basin, scale: 5000, })
 print(stats)
 print('Average Rainfall:', stats.get('precipitation_sum'))
 
-
+Map.setOptions('SATELLITE')
+Map.addLayer(total, visParams, 'Total Precipitation')
+Map.addLayer(basin, {}, 'Basin')
+Map.centerObject(basin)
 ////////////////////////////////////////////////////////////////// ANUAL RAINFALL BY COUNTRY  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 var chirps = ee.ImageCollection('UCSB-CHG/CHIRPS/DAILY')
@@ -157,6 +151,7 @@ var makeLegend = function(colors, labels) {
   return ui.Panel(entry);
 };
 
+Map.setOptions('SATELLITE')
 Map.centerObject(pakistan, 4);
 Map.addLayer(clippedPrecipitation, precipitationVis, 'Annual Precipitation (mm)');
 legend.add(makeLegend(legendColors, legendLabels));
